2389ca027d8c7048388ab246be381401ada513d3
sowens99 <seanowens@comcast.net>
2021-06-08 21:50:24 -0400

    Update plane_physics.h

--- plane_physics.h
+++ plane_physics.h
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 struct Vector {
     float x;
     float y;
     float z;
 };
-
 
 struct MotStruct {
     uint32_t flags;
@@ -19,24 +19,26 @@ struct MotStruct {
     float index9;
 };
 
-struct hitobj {
-    uint32_t attr;
-};
-
 struct Player {
     struct Vector wRotationRelated;
     float wDirectionView;
     struct Vector position;
     double dispDirectionCurrent;
     struct MotStruct motStruct;
-    uint8_t wStickDir1;
+    uint8_t stickPosition;
     float baseSpeed;
 };
 
+// Returns from main sim function
+struct Result {
+    struct Player player;
+    bool collided;
+    bool landed;
+};
+
 struct Player getPlayer();
-void initPlayerStruct();
-void init();
+struct Player init();
 double revise360(double param_1);
 void sincosf(float __x, float *__sinx, float *__cosx);
 int sysMsec2Frame(int param_1);
-int frame();
+void frameSim(signed char stickPosition, int numFrames, struct Player *previousFrame, struct Result *nextFrame);
