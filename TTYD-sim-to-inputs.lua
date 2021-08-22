ecaceb46e697fc3cb4e9b418a9c7e47a8e5575e1
sowens99 <seanowens@comcast.net>
2021-08-22 01:29:20 -0400

    Read un-modified simRes.txt (use most recent generation's inputs)
    Previously, I needed to remove previous generations, then remove the first 10 values in the file, as this was either header data or the 0th frame's data which should be ignored. Now the exported file can be used immediately for this script.

--- TTYD-sim-to-inputs.lua
+++ TTYD-sim-to-inputs.lua
@@ -6,18 +6,20 @@ function onScriptStart()
 	startFrame = GetFrameCount()
 	for line in io.lines("simRes.txt") do
 		lines = line .. ','
-		text = ''
-		for x in lines:gmatch("(.-),") do
-			table.insert(delimited, x)
-		end
-		
-		-- Every fifth value is the input
-		for x=6,#delimited,5 do
-			table.insert(inputs, delimited[x])
-		end
-		
-		return
 	end
+	
+	-- Put comma-delimited values into a table
+	for x in lines:gmatch("(.-),") do
+			table.insert(delimited, x)
+	end
+	
+	-- Skip over the first 10 values in the table
+	-- Then, every fifth value is the input
+	for x=11,#delimited,5 do
+		table.insert(inputs, delimited[x])
+	end
+	
+	return
 end
 
 function onScriptCancel()
@@ -27,17 +29,23 @@ function stickToInput(stick)
 	-- This function assumes unsigned stick values because I am lazy
 	-- 184 to 255 is left (to center)
 	-- 1 to 72 is right
-	if stick >= 184 then
-		return stick - 143
-	else
-		return stick + 143
+	input = 128
+	if stick < 0 then
+		input = input - 15 + stick
+	elseif stick > 0 then
+		input = input + 15 + stick
 	end
+	return input
 end
 
 function onScriptUpdate()
-	curFrame = GetFrameCount() - startFrame + 1
-	MsgBox(curFrame)
-	stick = tonumber(inputs[curFrame])
+	curFrame = GetFrameCount() - startFrame
+	if curFrame+1 > #inputs then
+		MsgBox("Collided.")
+		CancelScript()
+		return
+	end
+	stick = tonumber(inputs[curFrame+1])
 	input = stickToInput(stick)
 	SetMainStickX(input)
 end
