54e68590836d0a04c34cb34f0f88472bf835c7a9
Charles McAnany <git@charlesmcanany.com>
2021-08-22 15:17:36 -0500

    generate_playerdats half-way updated

--- generate_playerdats.zsh
+++ generate_playerdats.zsh
@@ -5,8 +5,21 @@ export_pd () {
             --ram rams/$1.ram \
             --landingX $2 \
             --landingY $3 \
+            --platformX $4 \
             >playerdats/$1.dat
 }
+
+export_pd_int () {
+    python3 ./export_player.py \
+        --ram rams/$1.ram \
+        --landingX $2 \
+        --landingY $3 \
+        --platformX $4 \
+        --interferenceX1 $5 \
+        --interferenceX2 $6 \
+        --interferenceY $7 \
+        >playerdats/$1.dat
+} 
 
 export_pd tyd_east_room -468 110
 export_pd flurrie 482 100
